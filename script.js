70fff61f5d48c62d264cbcd3e935aede145bcf84
Akeops <lalanne.andoni1@gmail.com>
2024-01-24 12:48:56 +0100

    Avancement du projet

--- script.js
+++ script.js
@@ -16,7 +16,24 @@ const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
-const weapons = [];
+const weapons = [
+    {
+      name: "stick",
+      power: 5
+    },
+    {
+      name: "dagger",
+      power: 30
+    },
+    {
+      name: "claw hammer",
+      power: 50
+    },
+    {
+      name: "sword",
+      power: 100
+    }
+  ];
 const locations = [
     {
         name: "town square",
@@ -70,7 +87,24 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-
+    if (currentWeapon < weapons.length - 1) {
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeapon++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeapon].name;
+            text.innerText = "You now have a " + newWeapon + "."; 
+            text.innerText += " In your inventory you have: " + inventory;
+            inventory.push(newWeapon);
+        } else {
+            text.innerText = "You do not have enough gold to buy a weapon.";
+        }
+    } else {
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
+    }
+    
 }
 
 function update(location) {
